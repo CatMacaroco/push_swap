1d3acabc602f17f9529e8b85f42bfa4e8a172f19
Catarina Macaroco <cmacaroc@f0r1s12.codam.nl>
2025-12-19 15:06:27 +0100

    rotate & push

--- command_push.c
+++ command_push.c
@@ -6,7 +6,7 @@
 /*   By: cmacaroc <cmacaroc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/16 18:55:17 by cmacaroc          #+#    #+#             */
-/*   Updated: 2025/12/16 20:23:23 by cmacaroc         ###   ########.fr       */
+/*   Updated: 2025/12/19 14:37:24 by cmacaroc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,15 +19,23 @@ void pa(t_list **a, t_list **b)
 	
 	t_list *temp;
 	
-	a = malloc(sizeof(t_list));
-	if(!a)
-		return(NULL);
+	temp = *b;
+	*b = (*b)->next; //head of b is now b->next(next node)
+	temp->next = *a; //move a to next node
+	*a = temp;			//now head of a is b.
 	
-	*a->data = *b->data;
 	
+}
+
+void pb(t_list **a, t_list **b)
+{
+	if(!a || !*a)
+		return;
+	
+	t_list *temp;
+
 	temp = *a;
 	*a = (*a)->next;
-	
-	
-	
+	temp->next = *b;
+	*b = temp;
 }
--- command_rotate.c
+++ command_rotate.c
@@ -6,8 +6,54 @@
 /*   By: cmacaroc <cmacaroc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/16 18:55:17 by cmacaroc          #+#    #+#             */
-/*   Updated: 2025/12/16 18:55:45 by cmacaroc         ###   ########.fr       */
+/*   Updated: 2025/12/19 15:05:46 by cmacaroc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+void ra(t_list **a, int print)
+{
+
+	t_list *first;
+	t_list *last;
+	
+	if(!a || !*a || (*a)->next == NULL)
+		return;
+	
+	
+	first = *a;
+	*a = (*a)->next;
+	
+	last = *a;
+	while(last->next)
+		last = last->next;
+	
+	last->next = first;
+	first->next = NULL;
+}
+
+void rb(t_list **b, int print)
+{
+	t_list *first;
+	t_list *last;
+	
+	if(!b || !*b || (*b)->next == NULL)
+		return;
+	
+	first = *b;
+	*b = (*b)->next;
+	
+	last = *b;
+	while(last->next)
+		last = last->next;
+		
+	last->next = first;
+	first->next = NULL;
+}
+
+void rr(t_list **a, t_list **b, int print)
+{
+	ra(&a, 0);
+	rb(&b, 0);
+}
