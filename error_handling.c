2b5e7b0a068ccd9a87b9a2c82a4621da8e4ef76b
Catarina Macaroco <cmacaroc@f0r3s21.codam.nl>
2025-12-22 15:48:23 +0100

    parsing

--- error_handling.c
+++ error_handling.c
@@ -6,18 +6,33 @@
 /*   By: cmacaroc <cmacaroc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/22 15:00:53 by cmacaroc          #+#    #+#             */
-/*   Updated: 2025/12/22 15:02:32 by cmacaroc         ###   ########.fr       */
+/*   Updated: 2025/12/22 15:48:15 by cmacaroc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-cvoid check_error(char *str)
+void check_error(char *str)
 {
+	int i = 0;
 
+	if(!str || !str[0])
+	{
+		ft_error();
+		exit(EXIT_FAILURE);
+	}
+	while(str[i])
+	{
+		if(str[i] < '0' || str[i] > '9')
+		{
+			ft_error();
+			exit(EXIT_FAILURE);
+		}
+		i++;
+	}	
 }
 
-void check_duplicates(t_list *node)
+void check_duplicates(t_list *stack)
 {
 	
 }
